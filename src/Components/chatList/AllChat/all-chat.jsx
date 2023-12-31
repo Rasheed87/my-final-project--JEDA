ac818eb7309d57e5eb6ae568ebe3dbe0f7d1d849
ahmed project <ahmedosigbeleme112@gmail.com>
2023-12-31 11:20:12 +0100

    created AllChat Component

---
+++ src/Components/chatList/AllChat/all-chat.jsx
@@ -0,0 +1,72 @@
+import "./all-chat.css";
+import User from "../Pinned-chat/userChat/user";
+import pix from "./images/photo.png";
+
+const AllChat = () => {
+  let users = [
+    {
+      index: 0,
+      photo: { pix },
+      name: " Luqman Ajiboye",
+      text: "I texted you last nigh",
+      time: "8:14",
+    },
+    {
+      index: 2,
+      photo: { pix },
+      name: " Titi Ayo",
+      text: "you are welcome",
+      time: "1:22",
+    },
+    {
+      index: 3,
+      photo: { pix },
+      name: " Faith Bamilola",
+      text: "good afternoon",
+      time: "6:42",
+    },
+    {
+      index: 4,
+      photo: { pix },
+      name: " Aishat Ahmed",
+      text: " call me",
+      time: "9:26",
+    },
+    {
+      index: 5,
+      photo: { pix },
+      name: " Olaolu Kolawole",
+      text: "we are breaking up",
+      time: "11:18",
+    },
+    {
+      index: 6,
+      photo: { pix },
+      name: "Ekpu Karo",
+      text: "i love you",
+      time: "11:18",
+    },
+    {
+      index: 7,
+      photo: { pix },
+      name: " Sikiru Alo",
+      text: "i dont care",
+      time: "11:18",
+    },
+  ];
+  return (
+    <>
+      <div className="allchat">
+        <h1 className="allchat1">All Chats</h1>
+      </div>
+
+       {/* mapping through the users list */}
+        {users.map((user, index) => (
+          <User key={index} {...user} />
+        ))}
+      
+    </>
+  );
+};
+
+export default AllChat;
